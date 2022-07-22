55a41520debc66bf178f97f50e07647a0a56f0e3
97hdz <70217263+97hdz@users.noreply.github.com>
2022-07-22 20:57:20 +0200

    FlatList component + API images

--- movies-app/src/components/DiscoverMovies.tsx
+++ movies-app/src/components/DiscoverMovies.tsx
@@ -1,22 +1,47 @@
 import React, { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import { fetchData } from '../services/API'
-import axios, { AxiosResponse } from 'axios'
+import { AxiosResponse } from 'axios'
+import { IMG_URL } from '../config/config'
+import { FlatList } from 'react-native-gesture-handler'
+import { TabBarItem } from 'react-native-tab-view'
 
 const DiscoverMovies = () => {
-    const [movies, SetMovies] = useState<{}[]>();
+    const [movies, setMovies] = useState<[]>([]);
+    const [images, setImages] = useState<[]>([]);
 
     useEffect(() => {
         const getMovies = async () => {
             const response: AxiosResponse | undefined = await fetchData('discover/movie')
-            console.log(response?.data.results)
+            const results: [] = response?.data.results
+            // console.log(results)
+            setMovies(results)
+            //getting all the path fot he images
+            const img = response?.data.results.map((data: any, index: any) => `${IMG_URL}${data.backdrop_path}`)
+            setImages(img)
         }
         getMovies()
     }, [])
 
+    console.log(movies)
+    // console.log(images)
+
     return (
         <View>
-            <Text> Discover movies component </Text>
+            <FlatList
+                ListHeaderComponent={<Text></Text>}
+                horizontal={true}
+                data={movies}
+                renderItem={({ item } : any ) => (
+                    <Image
+                        style={{ height: 250, width: 150 }}
+                        source={{
+                            uri: `${IMG_URL}${item?.backdrop_path}`,
+                        }}
+                    />
+                )}
+            />
+            <Text></Text>
         </View>
     )
 }
--- movies-app/src/config/config.ts
+++ movies-app/src/config/config.ts
@@ -1,2 +1,3 @@
 export const API_KEY : string = 'cea4f28e3a1d56f9a1e0c5fe41521799'
 export const BASE_URL : string = 'https://api.themoviedb.org/3/'
+export const IMG_URL : string = 'https://image.tmdb.org/t/p/original'
